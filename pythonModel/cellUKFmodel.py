35da09066e9ae7aee4756c3cda39c2d938908433
shivam-bhadani-MOSartLabs <shivam.bhadani@mosartlabs.com>
2025-10-09 17:56:58 +0530

    added UKF steps to the UKF example

--- pythonModel/cellUKFmodel.py
+++ pythonModel/cellUKFmodel.py
@@ -91,6 +91,72 @@ class SoCUKFEstimator:
         self.Q = 0.1  #variance in SoC update lets calculate this
         #self.R = R  #variance in voltage/ocv measurements from lookup and noise/errors, lets 
         self.lambda_=0.1
+        self.Innovation=[]
+        self.Vcmin=3.5  #at 25C
+        self.Vcmax=4.25
+
+        """
+        Model Udate part-1: Update Qc Cell capacity
+        RLS estimator for battery capacity Qc.
+        :param initial_Qc: Initial guess for capacity in mAh
+        :param QcRLSmemory: Forgetting factor (close to 1 for slow aging)
+        :param sigma_Q: Std deviation of charge measurement noise (mAh)
+
+        self.Qc = self.variable_stats['Qc']['mean']  # Initial estimate of capacity
+        self.QcUncertainity = 16*self.variable_stats['Qc']['var']  # Initial uncertainty (scalar)
+        self.QcRLSmemory = 0.995  # may be optimized based on how fast/slow Qc ages/changes
+        self.Qvariance = 0  # Variance of Q(t) measurement
+        self.coulombCount=0
+        self.absCoulombCount=0
+        self.x_est_prev=self.x_est
+        self.coulombCount_prev=0
+        """
+        # RLS-based capacity estimation
+        self.Qc_charge = self.variable_stats['Qc']['mean']
+        self.Qc_discharge = self.variable_stats['Qc']['mean']
+        self.P_charge = 1e6
+        self.P_discharge = 1e6
+        self.QcRLSmemory = 0.99
+        self.sigma_Q = 5.0  # mAh noise in coulomb count
+        self.SoC_variance_threshold = 0.002
+        self.SoC_start = None
+        self.Q_accumulated = 0.0
+        self.mode = None  # 'charge' or 'discharge'
+        self.rls_active = False
+        self.min_delta_soc_factor = 5.0  # Minimum delta SoC = factor × sqrt(P)
+        self.min_delta_soc_forQcUpdate=0.01
+        self.qcUpdateMode= 1 # use min_delta, if 0 use the delta_soc_factor
+        self.chargeQcUpdateCount=0
+        self.dischargeQcUpdateCount=0
+        self.charge_soc=[]
+        self.discharge_soc=[]
+        self.initialCalibrationComplete=False
+        
+    def checkFuse(self, discharge):
+        #dt= time - self.time[-1]
+        #discharge=i*dt
+        if discharge==0:
+            return True
+        if discharge>0:
+            # We are discharging
+            dSoC=discharge/(self.variable_stats['eta']['mean']*self.variable_stats['Qc']['mean'])
+                        
+        else:
+            # We are charging
+            dSoC=discharge/self.variable_stats['Qc']['mean']
+
+        if (self.x_est-dSoC)>=0.99 and discharge<0:
+            self.charge=False
+            #print("Cannot charge anymore battery full. SoC",self.SoC[-1]," SoH=",self.SoH[-1]," time=",time)
+            return False
+        elif (self.x_est-dSoC)<=0.01 and discharge>0:
+            #print("Cannot discharge anymore battery empty. SoC",self.SoC[-1]," SoH=",self.SoH[-1]," time=",time)
+            self.discharge=False
+            return False
+        else:
+            self.charge=True
+            self.discharge=True
+            return True
 
 
     def _generate_sigma_points(self, x, P):
@@ -115,64 +181,177 @@ class SoCUKFEstimator:
             self.inner_term = [
                 ['I'],
                 ['-Id'] ]
-                           
+           
         self.Qcharge, self.Q = self.stats(self.common_factors, self.inner_term)
         self.x_pred = self.x_est - self.Qcharge
         self.P_pred = self.P + self.Q
 #        print("dQ=",self.Qcharge," post predict x_pred=",self.x_pred)
 
-    def update(self, V_k):
-        
-        sigma_points, weights = self._generate_sigma_points(self.x_pred, self.P_pred)
-        weights_mean = np.mean(weights)
-        #weights_cov = weights_mean.copy()
-        #Variance is v_noise+d_ocv+dI*z+I*dZ, since dI/I is much smaller than dZ/Z, ignore dI
-        deltaV=self.cellESR.calculateESR(self.variable_stats['I']['mean'],self.variable_stats['dt']['mean'])*self.variable_stats['I']['mean']
-        #print("deltaV=",deltaV," time=",self.cellESR.time)
-        self.R=self.vNoise**2+self.dOCV**2+(self.dZ**2)*abs(deltaV)
-        predicted_measurements = [self.socEstimator.estimateOCV(100*(1-sp),self.variable_stats['T']['mean'])+deltaV for sp in sigma_points]
-        
-        if (max(sigma_points)<1) and (min(sigma_points)>0):
-            z_pred = np.dot(weights, predicted_measurements)/np.sum(weights)
-            P_xz = sum(weights[i] * (sigma_points[i] - self.x_pred) * (predicted_measurements[i] - z_pred) for i in range(3))
-            P_zz = sum(weights[i] * (predicted_measurements[i] - z_pred)**2 for i in range(3))
-            slope=(P_xz / P_zz)
-        else:
-            z_pred=predicted_measurements[0]
-            P_zz = sum(weights[i] * (predicted_measurements[i] - z_pred)**2 for i in range(3))
-            if (max(sigma_points)<1):
-                slope=(predicted_measurements[0]-predicted_measurements[2])/(sigma_points[0]-sigma_points[2])                
-            else:
-                slope=(predicted_measurements[0]-predicted_measurements[1])/(sigma_points[0]-sigma_points[1])
-
-       # print("z_pred=",z_pred," predicted measurements=",predicted_measurements)
-        
-        
-
-        K = slope*(self.P_pred/(self.P_pred+self.R))
-        self.x_est=min(1,max(self.x_pred + K * (V_k - z_pred),0))
-        self.P = max(self.P_pred - K * P_zz * K, self.Q)
-        
-        x_est=self.x_pred + K * (V_k - z_pred)
-        P=self.P_pred - K * P_zz * K
-        if x_est>0 and x_est<1:
-            if self.x_est != x_est:
-                print("lets investigate x_est=",x_est," self.x_est=",self.x_est)
-            if P!= self.P:
-                print("lets investigate p=",P," self.P=",self.P)
-                print("self.P_pred=",self.P_pred," K=",K, "Pzz=",P_zz)
-        elif x_est>=1:
-            if self.x_est !=1:
-                print("lets investigate why x_est not clipped x_est=",x_est," self.x_est=",self.x_est)
-            #don't update self.P
-        else:
-            if self.x_est !=0:
-                print("lets investigate why x_est not clipped x_est=",x_est," self.x_est=",self.x_est)
-            
-            
-            
-        #print("K=",K," Slope=",slope)
-        #print("v_k=",V_k," Zpred=",z_pred)
-        #print("post update x_est=",self.x_est," x_pred=",self.x_pred)
+    def update(self, V_k, Qaccumulated):
+         sigma_points, weights = self._generate_sigma_points(self.x_pred, self.P_pred)
+         #weights_mean = np.mean(weights)
+         #weights_cov = weights_mean.copy()
+         #Variance is v_noise+d_ocv+dI*z+I*dZ, since dI/I is much smaller than dZ/Z, ignore dI
+         deltaV=self.cellESR.calculateESR(self.variable_stats['I']['mean'],self.variable_stats['dt']['mean'])*self.variable_stats['I']['mean']
+         #print("deltaV=",deltaV," time=",self.cellESR.time)
+         self.R=self.vNoise**2+self.dOCV**2+(self.dZ**2)*abs(deltaV)
+         predicted_measurements = [self.socEstimator.get_ocv(100*(1-sp),self.variable_stats['T']['mean'])+deltaV for sp in sigma_points]
+         #print("x0=",sigma_points[0]," OCV=",self.socEstimator.get_ocv(100*(1-sigma_points[0]),self.variable_stats['T']['mean']), " DeltaV=",deltaV, "Vpred=",predicted_measurements)
+         
+         
+         if (max(sigma_points)<1) and (min(sigma_points)>0):
+             z_pred = np.dot(weights, predicted_measurements)/np.sum(weights)
+             P_xz = sum(weights[i] * (sigma_points[i] - self.x_pred) * (predicted_measurements[i] - z_pred) for i in range(3))
+             P_zz = sum(weights[i] * (predicted_measurements[i] - z_pred)**2 for i in range(3))
+             slope=(P_xz / P_zz)
+         else:
+             z_pred=predicted_measurements[0]
+             P_zz = sum(weights[i] * (predicted_measurements[i] - z_pred)**2 for i in range(3))
+             if (max(sigma_points)<1):
+                 slope=(predicted_measurements[0]-predicted_measurements[2])/(sigma_points[0]-sigma_points[2])                
+             else:
+                 slope=(predicted_measurements[0]-predicted_measurements[1])/(sigma_points[0]-sigma_points[1])
+ 
+        # print("z_pred=",z_pred," predicted measurements=",predicted_measurements)
+         #print("slope=",slope)
+         
+ 
+         K = slope*(self.P_pred/(self.P_pred+self.R))
+         
+         #K = slope
+         self.x_est=min(1,max(self.x_pred + K * (V_k - z_pred),0))
+         self.P = max(self.P_pred - K * P_zz * K, self.Q)
+                
+         self.Innovation.append([self.x_pred,self.x_est,slope,K,V_k,z_pred])    
+         self.update_capacity_estimate(Qaccumulated)
+             
+         #print("K=",K," Slope=",slope)
+         #print("v_k=",V_k," Zpred=",z_pred)
+         #print("post update x_est=",self.x_est," x_pred=",self.x_pred)
     def get_estimate(self):
         return self.x_est
+    
+    def start_cycle(self, mode, Q):
+        self.SoC_start = self.x_est
+        self.Q_offset = Q
+        self.mode = mode  # 'charge' or 'discharge'
+
+        self.rls_active = True
+        print("started Qc RLS soc=",self.SoC_start," Qoffset=",self.Q_offset)
+    
+    def reset_cycle(self):
+        self.SoC_start = None
+        self.rls_active = False
+        self.updateCount=None
+        print("stopped Qc RLS")
+        
+    def update_capacity_estimate(self, Qaccumulated):
+        # Check if SoC variance is low enough to trust
+   
+        if self.rls_active and self.SoC_start is not None:
+            if ((self.mode=="charge") and (self.variable_stats['I']['mean']>0)) or ((self.mode=="discharge") and (self.variable_stats['I']['mean']<0)):
+                self.reset_cycle()
+            else:            
+                delta_SoC = self.x_est - self.SoC_start
+                if self.qcUpdateMode:
+                    dsocThreshold = self.min_delta_soc_forQcUpdate
+                else:
+                    dsocThreshold = self.min_delta_soc_factor * np.sqrt(self.P)
+                #print("dsocthreshold=",dsocThreshold)
+                if abs(delta_SoC) > dsocThreshold:  # Minimum observable change
+                    phi = delta_SoC
+                    y = Qaccumulated-self.Q_offset
+                    if self.mode == 'charge':
+                        denom = self.P_charge * phi**2 + self.sigma_Q**2
+                        K = (self.P_charge * phi) / denom
+                        error = y - phi * self.Qc_charge
+                        self.Qc_charge += K * error
+                        self.P_charge = (self.P_charge - K * phi * self.P_charge) / self.QcRLSmemory
+                        self.chargeQcUpdateCount+=1
+                        self.charge_soc.append([phi, y])
+                        #print(self.mode,"SoC=",self.x_est,"dQ=",Qaccumulated," Qc=",self.Qc_discharge)
+                        #self.variable_stats['eta']['mean']=-self.Qc_charge
+                        #self.variable_stats['Qc']['var']=-self.P_charge                        print(self.mode,"SoC=",self.x_est," Qc=",self.Qc_discharge)
+                    elif self.mode == 'discharge':
+                        denom = self.P_discharge * phi**2 + self.sigma_Q**2
+                        K = (self.P_discharge * phi) / denom
+                        error = y - phi * self.Qc_discharge
+                        self.Qc_discharge += K * error
+                        self.P_discharge = (self.P_discharge - K * phi * self.P_discharge) / self.QcRLSmemory
+                        self.dischargeQcUpdateCount+=1
+                        self.discharge_soc.append([phi, y])
+                        #print(self.mode,"SoC=",self.x_est,"dQ=",Qaccumulated," Qc=",self.Qc_discharge)
+                        #self.variable_stats['Qc']['mean']=-self.Qc_discharge
+                        #self.variable_stats['Qc']['var']=self.P_discharge
+                        #print(self.mode,"SoC=",self.x_est," Qc=",self.Qc_discharge)
+                    self.SoC_start=self.x_est
+                    self.Q_offset=Qaccumulated
+
+                    self.update_capacity_estimates()
+
+    def update_capacity_estimates(self):
+        efficiency =  self.Qc_discharge / self.Qc_charge if self.Qc_charge != 0 else None
+        if (self.dischargeQcUpdateCount>3):
+            self.variable_stats['Qc']['mean']=-self.Qc_discharge
+            self.variable_stats['Qc']['var']=self.P_discharge
+            print("Updated Qc=",self.variable_stats['Qc']['mean'])
+            if (self.chargeQcUpdateCount>3):
+                self.variable_stats['eta']['mean']=efficiency
+                print("Updated Eta=",self.variable_stats['eta']['mean'])
+        return {
+            "Qc_charge": self.Qc_charge,
+            "Qc_discharge": self.Qc_discharge,
+            "efficiency": efficiency
+        }
+
+    def start_calibration_cycle(self, V, T, mode):
+        self.calibration_soc_start = (100-self.socEstimator.get_dod(V, T))/100
+        self.calibration_Q = 0
+        self.calibration_cycle_data = []
+        self.calibration_mode = mode
+        self.calibration_active = True
+        self.slopeCheck_soc= self.calibration_soc_start
+        self.slopeCheck_ocv= V
+        self.scale_factor=1
+        self.slopes=[]
+ 
+                    
+    def record_calibration_point(self, I, V, T, dt, slope_threshold=2, min_soc_span=0.01):
+        """
+        Record a calibration observation and check if slope-based anchor condition is met.
+        calibrationping completes only when dOCV/dSoC exceeds threshold.
+        """
+        dQ=I*dt
+        self.calibration_Q +=dQ
+        self.deltaV = self.cellESR.calculateDeltaV(I,dt)
+        V_ocv = V - self.deltaV
+        SoC = self.calibration_soc_start - (self.calibration_Q/self.variable_stats['Qc']['mean'])
+        self.calibration_cycle_data.append((SoC, V_ocv, T))
+        #if V_ocv>self.Vcmax:
+            #print("Reached Max Voltage")
+    
+        if abs(self.slopeCheck_soc-SoC)>min_soc_span:
+            # Sort by SoC
+            slope=(self.slopeCheck_ocv-V_ocv)/(self.slopeCheck_soc-SoC)
+            self.slopes.append(slope)
+            self.slopeCheck_ocv=V_ocv
+            self.slopeCheck_soc=SoC
+            soc_now=max(min(1,(100-self.socEstimator.get_dod(V_ocv,T))/100),0)
+            if len(self.slopes)>4:
+                if ((np.mean(self.slopes[-3:])>slope_threshold) and abs(self.calibration_soc_start-soc_now)>0.5)or(self.calibration_mode=="discharge" and V_ocv<self.Vcmin) or (self.calibration_mode=="charge" and V_ocv>self.Vcmax):               
+                    self.scale_factor = (self.calibration_soc_start - soc_now) / (self.calibration_soc_start - SoC)
+                    print(self.calibration_mode," rescaling factor=",self.scale_factor)
+                    rescaled_data = [
+                        (self.calibration_soc_start + (s - self.calibration_soc_start) * self.scale_factor, v, t)
+                        for s, v, t in self.calibration_cycle_data
+                    ]
+                    self.calibration_cycle_data=rescaled_data
+                    self.socEstimator.update_from_observations(rescaled_data)
+                    #self.socEstimator.update_ocv_grid_from_calibration(rescaled_data)
+                    self.x_est=soc_now
+                    print("Previous Qc=",self.variable_stats['Qc']['mean'])
+                    self.variable_stats['Qc']['mean']=abs((self.calibration_Q)/(self.calibration_soc_start - soc_now)) 
+                    print("Updated Qc=",self.variable_stats['Qc']['mean'])
+                    self.calibration_active = False
+                    print(f"✅ calibrationping complete: max slope = {slope:.3f} V/SoC, SoC swing = {(soc_now-self.calibration_soc_start):.2f}, scale = {self.scale_factor:.2f}, points = {len(rescaled_data)}")
+                
--- pythonModel/evaluateUKFmodel.py
+++ pythonModel/evaluateUKFmodel.py
@@ -14,24 +14,55 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def fuseCheck(battery, current):
+def fuseCheck(battery,battery_model,current,time):
     fuse=True
-    for cell in battery:
-        fuse = fuse and cell.checkFuse(time,current)
+    for cell, cell_model in zip(battery,battery_model):
+        fuse = fuse and cell.checkFuse(time*current) and cell_model.checkFuse(time*current) 
     return fuse
-    
+
+def calibrate(battery,battery_model,current,Temperature):
+    if current>0:
+        mode="discharge"
+    elif current <0:
+        mode="charge"
+    else:
+        print("Error, calibrating with zero current!!")
+        return
+    for cell, cell_model in zip(battery,battery_model):
+        cell(0,0,Temperature)                       
+        voltage_measured=cell.voltage[-1] + np.random.normal(loc=0.0, scale=dVoltage/10, size=None)  #10 since we expect atleast 100x averaging due to 100Hz measurements
+        cell_model.start_calibration_cycle(voltage_measured,Temperature,mode)
+    coulombCounter_discharge=0
+    battery_calibrate=True
+    deltaT=1  #time step is 1 sec to reduce simulation time
+    t=0
+    while (battery_calibrate):
+        current_measured=current*(1+np.random.normal(loc=0.0, scale=dCurrent, size=None))
+        coulombCounter_discharge += current_measured * deltaT
+        t+=deltaT
+        battery_calibrate=False
+        for j, (cell, cell_model) in enumerate(zip(battery,battery_model)):
+            cell(deltaT,current,Temperature)                       
+            voltage_measured=cell.voltage[-1] + np.random.normal(loc=0.0, scale=dVoltage, size=None)
+            temperature_measured=Temperature + np.random.normal(loc=0.0, scale=dTemperature, size=None)
+            if cell_model.calibration_active:
+                cell_model.record_calibration_point(current_measured, voltage_measured,temperature_measured, deltaT)
+            battery_calibrate=battery_calibrate or cell_model.calibration_active
+    return t
+
+
 
 #initial_soc, capacity, eta, Id, Q, R
 
 battery_stats = {
-    'dt':   {'mean': 1, 'var': 1e-8},
+    'dt':   {'mean': 1, 'var': 0},
     'I':    {'mean': 0, 'var': 0}
 # period of IV measurements, jitter in IV mesurements
 #I and (0.0005*I)**2
 }
     
 cell_stats = {
-    'Qc':   {'mean': 65*3600, 'var': 0.1*0.1*65},
+    'Qc':   {'mean': 65*3600, 'var': 9*0.1*0.1*65*65*3600*3600},
     'eta':  {'mean': 0.98, 'var': 0.01*0.01},
     'Id':   {'mean': 1e-3, 'var': 0.25e-6},
     'T' :   {'mean': 25, 'var': 0.01},
@@ -40,6 +71,8 @@ cell_stats = {
 R0=cell_stats['R0']['mean'] #ohms
 RZlist=[[1e-3,10,0],[1.5e-3,1e4,0],[8,5e-7,1]]
 ocvLookup="Sample_OCV_DoD.csv"
+ocvLookup="lowCornerCalib_ocvTable.csv"
+calibrate=False
 
 dCurrent=0.0005 # sigma for current measurement
 dVoltage=5/64000  # 15bit enob for 1 sigma
@@ -55,14 +88,28 @@ soc_true=[]
 soc_est=[]
 P_values=[]
 I_values=[]
+SoC_LastQcUpdate=[]
 for i, corner in enumerate(corners):
     battery.append(Cell("Li_Ion", R0, RZlist, corner=corner))
+    battery[i].ocvLookup=ocvLookup
     battery_model.append(SoCUKFEstimator(battery_stats, cell_stats, RZlist, ocvLookup))
     soc_true.append([])
     soc_est.append([])
     P_values.append([])
-    I_values.append([])
+    SoC_LastQcUpdate.append(battery_model[-1].x_est)
            
+
+if calibrate:
+    Temperature=25
+    #2C discharge
+    tsimulated=calibrate(battery,battery_model,130,Temperature)
+    #1C charge
+    tsimulated+=calibrate(battery,battery_model,-65,Temperature)
+else:
+    tsimulated=0
+    
+    #battery_model[0].socEstimator.export_updated_grid_to_csv("lowCornerCalib_ocvTable.csv")
+
 input_csv = 'test_sequence.csv'
 #input_csv = 'la92shortdds.csv'
 data = pd.read_csv(input_csv)
@@ -71,24 +118,30 @@ time_measured = data['Time'].values[offset:]
 current = data['Current'].values[offset:]
 temperature= data['Temperature'].values[offset:]
 
-
-max_loop=10000
+max_loop=30000
 loop_count=0
 charge=0
-previous_t=0
+previous_t=tsimulated
 t_model=0
 previous_fuse=True
+dSoC_QcUpdate=0.1
+t_measured_prev=0
+coulombCount=0
+absCoulombCount=0
+started_QcRLS=False
+toffset=tsimulated
 for i, time in enumerate(time_measured):
     if i!=0:
-        start_t=time_measured[i-1]
-        for nclk in range(int((time-previous_t)/battery_stats['dt']['mean'])):
+        for nclk in range(int((time+toffset-previous_t)/battery_stats['dt']['mean'])):
             loop_count=loop_count+1
             if loop_count>max_loop:
                 break            
-            t=time_measured[i-1] + nclk*battery_stats['dt']['mean']+np.random.normal(loc=0.0, scale=np.sqrt(battery_stats['dt']['var']), size=None)
+            t=toffset+time_measured[i-1] + nclk*battery_stats['dt']['mean']+np.random.normal(loc=0.0, scale=np.sqrt(battery_stats['dt']['var']), size=None)
+            t_measured=toffset+time_measured[i-1] + nclk*battery_stats['dt']['mean']          
             current_now=current[i-1]+(current[i]-current[i-1])*nclk*battery_stats['dt']['mean']/(time-previous_t)
             current_measured=current_now*(1+np.random.normal(loc=0.0, scale=dCurrent, size=None))
-            if not(fuseCheck(battery,current_now)):
+            fuse_blown=not(fuseCheck(battery,battery_model,current_now,battery_stats['dt']['mean']))
+            if fuse_blown:
                 #print("protection fuse blown, run simulation with zero current since protection FET is OFF, current=", current_now, " first cell SoC=", battery[0].SoC[-1])
                 current_now=0
                 current_measured=0
@@ -98,13 +151,20 @@ for i, time in enumerate(time_measured):
                 break
             else:
                 if not(previous_fuse):
-                    print("fuse recovered at time=",t,"current=", current_now, " first cell SoC=", battery[0].SoC[-1])           
+                    print("fuse recovered at time=",t,"current=", current_now, " first cell SoC=", battery[0].SoC[-1])
+                    t_measured_prev=t_measured-battery_stats['dt']['mean'] 
                 previous_fuse=True
                 
             temperature_now=temperature[i-1]+(temperature[i]-temperature[i-1])*nclk*battery_stats['dt']['mean']/(time-previous_t)
             temperature_measured=temperature_now+np.random.normal(loc=0.0, scale=dTemperature, size=None)         
+            coulombCount+=(t_measured-t_measured_prev)*current_measured
+            absCoulombCount+=abs((t_measured-t_measured_prev)*current_measured)
+            t_measured_prev=t_measured
+            I_values.append(current_measured)
+            #if current_measured<0:
+                #print("debug now")
             for j, (cell, cell_model) in enumerate(zip(battery,battery_model)):
-                cell(t,current_now,temperature_now)                        
+                cell(battery_stats['dt']['mean'],current_now,temperature_now)                       
                 voltage_measured=cell.voltage[-1] + np.random.normal(loc=0.0, scale=dVoltage, size=None)
                 battery_stats['I']['mean']=current_measured
                 #battery_stats['I']['mean']=current_now                
@@ -112,15 +172,25 @@ for i, time in enumerate(time_measured):
                 cell_stats['T']['mean']=temperature_measured                    
                 # Run the UKF steps
                 cell_model.predict()
-                cell_model.update(voltage_measured)
+                cell_model.update(voltage_measured,coulombCount)
                 soc_true[j].append(cell.SoC[-1])
                 soc_est[j].append(100*cell_model.get_estimate())
                 P_values[j].append(cell_model.P)
-                I_values[j].append(current_measured)
+
+                if not(cell_model.rls_active):
+                    if len(P_values[j])>10:
+                        if max(P_values[j][-10:])<1e-3:
+                            if  all(i>0 for i in I_values[-10:]):
+                                cell_model.start_cycle("discharge",coulombCount)
+                            elif all(i<0 for i in I_values[-10:]):
+                                cell_model.start_cycle("charge",coulombCount)
+
     previous_t=time    
     if loop_count>max_loop:
         break
-
+column_names=['x_pred','x_est','slope','K','V_observed','v_pred']
+df = pd.DataFrame(battery_model[0].Innovation, columns=column_names)
+df.to_csv('Innovation_output.csv', index=False)
 
 for i, cell in enumerate(battery):
     #print(cell)
@@ -135,4 +205,3 @@ for i, cell in enumerate(battery):
     plt.plot(P_values[i])
     plt.title('Covariance P Over Time')
     plt.xlabel('Time Step')
-    plt.show()
